13371dfe95112a89f3ae9da1500be8039376c849
Dominic Muttel <dominic.muttel@gmail.com>
2015-09-14 17:16:50 -0600

    forms, tables, and logic added

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -3,3 +3,35 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+$('form').on('submit', function(e){
+  e.preventDefault();
+  var payload = {
+    name: $('#name').val(),
+    age: $('#age').val(),
+    spitter: $('#spitter').val()
+  };
+  if($('#spitter').is(':checked')){
+    payload.spitter = true;
+  } else {
+    payload.spitter = false;
+  }
+
+  $.post('/api/llamas', payload, function(data){
+    $('.message-section').show();
+    $('#message').html("Llama has been added, yo!");
+  getLlamas();
+  });
+});
+
+function getLlamas(){
+  $('#all-llamas').empty();
+  $.get('/api/llamas', function(data){
+    for (var i = 0; i < data.length; i++) {
+      $('#all-llamas').append(
+        '<tr><td>' + data[i].name + '</td><td>' + data[i].age + '</td><td>' + data[i].spitter + '</td></tr>');
+      $('form input').val('');
+      $('#spitter').removeAttr('checked');
+    }
+  });
+}
